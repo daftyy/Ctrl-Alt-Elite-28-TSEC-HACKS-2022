9ffe7d4d61723dbf09a36f139d6f4b3607c921f4
punIN10ded <imnotfreenow@gmail.com>
2022-03-09 15:00:00 +0530

    added dashboard page

---
+++ src/invite.js
@@ -0,0 +1,34 @@
+var acceptInvite = document.getElementById("accept");
+var declineInvite = document.getElementById("decline");
+
+declineInvite.onclick = decInc;
+acceptInvite.onclick = AccInc;
+
+function decInc() {
+	console.log("Declined Invite");
+}
+
+function AccInc() {
+	console.log("Accepted Invite");
+}
+
+// document.onclick = check;
+// function check(e) {
+// 	var target = (e && e.target) || (event && event.srcElement);
+
+// 	//Nav Menu
+// 	if (!checkParent(target, navMenuDiv)) {
+// 		// click NOT on the menu
+// 		if (checkParent(target, navMenu)) {
+// 			// click on the link
+// 			if (navMenuDiv.classList.contains("hidden")) {
+// 				navMenuDiv.classList.remove("hidden");
+// 			} else {
+// 				navMenuDiv.classList.add("hidden");
+// 			}
+// 		} else {
+// 			// click both outside link and outside menu, hide menu
+// 			navMenuDiv.classList.add("hidden");
+// 		}
+// 	}
+// }
